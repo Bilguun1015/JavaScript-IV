5b8a408e742c4e6ac19f44aa354232e7715202a4
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-06-13 14:58:23 -0500

    still working on Lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,68 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributesPerson){
+        this.name = attributesPerson.name;
+        this.age = attributesPerson.age;
+        this.location = attributesPerson.location;
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(attributesInstructor){
+        super(attributesInstructor);
+        this.specialty = attributesInstructor.specialty;
+        this.favLanguage = attributesInstructor.favLanguage;
+        this.catchPhrase = attributesInstructor.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student,subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
+
+class Student extends Person{
+    constructor(attributesStudent){
+        super(attributesStudent);
+        this.previousBackground = attributesStudent.previousBackground;
+        this.className = attributesStudent.className;
+        this.favSubjects = attributesStudent.favSubjects;
+    }
+    listsSubjects(){
+        console.log(this.favSubjects);
+    }
+    PRAssignment(subject){
+        console.log(`${student.name} has submitted a PR for ${subject}.`);
+    }
+    sprintChallenge(subject){
+        console.log(`${student.name} has begun sprint challenge on ${subject}.`);
+    }
+}
+
+class ProjectManagers extends Instructor {
+    constructor(attributesPM){
+        super(attributesPM);
+        this.gradClassName = attributesPM.gradClassName;
+        this.favInstructor = attributesPM.favInstructor;
+    }
+    standUp(channel){
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+    debugsCode(){
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+    }
+}
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+  fred.demo(math);
